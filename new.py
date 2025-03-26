321d15b246714d646acd85b20622e42f5ce62586
unknown <229140@TECH.WYB.AC.LK>
2025-03-26 20:24:13 +0530

    first commit

---
+++ new.py
@@ -0,0 +1,8 @@
+num=1
+while(num<100):
+     print(num)
+     num=num+2
+
+        
+
+    
---
+++ task3.py
@@ -0,0 +1,11 @@
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+num3=int(input("enter num3:"))
+if(num1>num2):
+    if(num1>num3):
+      print("lager number is num1")
+elif(num2>num1):
+    if(num2>num3):
+        print("lager number is num2")
+else:
+    print("lager number is num 3")
